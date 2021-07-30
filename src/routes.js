04c4bfa912dcbc83870fba5e4ec5a9c763b85216
jmarcelobms <jmarcelo@winfit.com.br>
2021-07-30 15:50:37 -0300

    Commit

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,24 @@ router.get('/:trackId', async (req, res, next) => {
     try {
         const { trackId } = req.params
         const track = searchForMusic(trackId)
-        await stremmingAudio(req, res, track.trackId)
+        const { fileSize, stream } = await stremmingAudio(track.trackId)
+
+        const { range } = req.headers;
+        const size = fileSize;
+        const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+        const end = size - 1;
+        const chunkSize = (end - start) + 1;
+
+        res.set({
+            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'audio/mp3',
+        });
+
+        res.status(206);
+        
+        return stream.pipe(res)
     } catch (error) {
         next(error)
     }
--- src/stream/index.js
+++ src/stream/index.js
@@ -7,33 +7,19 @@ import { NotFound } from '../exeptions/NotFound.js'
 const getStat = promisify(fs.stat)
 const exists = promisify(fs.exists)
 
-export const stremmingAudio = async (request, response, trackId = "") => {
+export const stremmingAudio = async (trackId = "") => {
     const filePath = resolve(__dirname, 'src', 'musics', trackId)
 
     const trackExists = await exists(filePath)
+
     if (!trackExists)
         throw new NotFound('TRACKID_NOT_FOUND')
 
-
     const stat = await getStat(filePath)
-
-    const { range } = request.headers;
-    const { size } = stat;
-    const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
-    const end = size - 1;
-    const chunkSize = (end - start) + 1;
-
-    response.set({
-        'Content-Range': `bytes ${start}-${end}/${size}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunkSize,
-        'Content-Type': 'audio/mp3'
-    });
-
-    response.status(206);
-
     const stream = fs.createReadStream(filePath)
 
-    // stream the file
-    return stream.pipe(response)
+    return {
+        stream: stream,
+        fileSize: stat.size
+    }
 }
